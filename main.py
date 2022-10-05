5aea93a1bf40053cd4793b7676d64469a94ea041
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-10-05 18:24:54 +0900

    #5.5~5.7 done!

--- main.py
+++ main.py
@@ -10,5 +10,16 @@ if response.status_code != 200:
   print("Can't request website")
 else:
   soup = BeautifulSoup(response.text, "html.parser")
-  jops = soup.find_all('li', class_="feature")
-  print(jops)
+  jobs = soup.find_all('section', class_="jobs")
+  for job_section in jobs:
+    job_posts = job_section.find_all('li')
+    job_posts.pop(-1)
+    for post in job_posts:
+      anchors = post.find_all('a')
+      anchor = anchors[1]
+      link = anchor['href']
+      company, kind, region = anchor.find_all('span', class_="company")
+      title = anchor.find('span', class_='title')
+      print(company, kind, region, title)
+      print("///////////////////////")
+      print("///////////////////////")
